9cd0d765075b77ebb5d1b41afefffc0007579427
Svyatoslav Shareko <modek@mail.ru>
2025-02-23 13:53:43 +0300

    Jan's task is in the rocking chair

---
+++ Ян в качалке/comparison.py
@@ -0,0 +1,43 @@
+from time import process_time_ns
+
+import first_solution
+import second_solution
+
+
+def function_operation_time(func):
+
+    def wrapper(*args, **kwargs):
+        start = process_time_ns()
+        result_func = func(*args, **kwargs)
+        end = process_time_ns()
+        result = (f'Результат {func.__name__}\n'
+                  f'Входные данные {args}\n'
+                  f'Время работы {end - start} наносекунд\n')
+        return result
+
+    return wrapper
+
+
+@function_operation_time
+def first_option(n, k):
+    result = first_solution.friends_and_Jan_on_track(n, k)
+    return first_solution.output_left_and_right_borders(*result)
+
+
+@function_operation_time
+def second_option(n, k):
+    result = second_solution.friends_and_Jan_on_track(n, k)
+    return second_solution.output_left_and_right_borders(*result)
+
+
+# Первый тест с маленькими числами
+print(first_option(100, 20))
+print(second_option(100, 20))
+
+# Первый тест с средними числами
+print(first_option(100, 30))
+print(second_option(100, 30))
+
+# Первый тест с большими числами
+print(first_option(100, 90))
+print(second_option(100, 90))
